1607be8863322ff93986d87c6eb9e0e1b10749f3
twitu <bhanuka.ishan@gmail.com>
2018-08-21 21:15:15 +0530

    complete Compilation Engine and Jack Tokenizer

--- 10/src/CompilationEngine.py
+++ 10/src/CompilationEngine.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+from JackTokenizer import JackTokenizer
+
+
 class CompilationEngine:
     __slots__ = '_tokenizer', '_output_stream', '_xml_output', '_indentation', \
                 '_keyword_compile_function_dict', '_statement_compile_function_dict'
@@ -33,9 +39,11 @@ class CompilationEngine:
         }
         self.compile_class()
 
-    def advance_and_print(self, terminate):
+    def advance_and_print(self, terminate=(), count=-1):
         # advances and prints terminals until it encounters termination character
-        while self._tokenizer.advance():
+        # also can specify number of tokens to print
+        # by default it will keep tokenizing until terminating character
+        while count != 0 and self._tokenizer.advance():
             token_type = self._tokenizer.token_literal_name[self._tokenizer.token_type()]
             token = self._tokenizer.return_token_value()
             self._xml_output.append(' ' * self._indentation +
@@ -44,49 +52,65 @@ class CompilationEngine:
                                         token,
                                         token_type)
                                     )
-            if token == terminate:  # appends termination character string to xml output
+            count -= 1
+            # appends termination character string to xml output
+            if token in terminate:  # finishes on finding termination token
                 return
 
     def compile_class(self):
         self._xml_output.append("<class>")
         self._indentation += 2
-        self.class_compile_var_dec()
-        self.compile_subroutine()
+        self.advance_and_print(['{'])
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() in ["static", "field"]:
+            self._tokenizer.go_back()
+            self.class_compile_var_dec()
+        else:
+            self._tokenizer.go_back()
+
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() in \
+                ["constructor", "function", "method"]:
+            self._tokenizer.go_back()
+            self.compile_subroutine()
+        else:
+            self._tokenizer.go_back()
+
+        self.advance_and_print(['}'])
         self._indentation -= 2
         self._xml_output.append("</class>")
 
     def class_compile_var_dec(self):
         self._xml_output.append(' ' * self._indentation + "<classVarDec>")
         self._indentation += 2
-        self.advance_and_print(';')
+        self.advance_and_print([';'])
         self._indentation -= 2
         self._xml_output.append(' ' * self._indentation + "</classVarDec>")
 
     def compile_subroutine(self):
         self._xml_output.append(' ' * self._indentation + "<subroutineDec>")
         self._indentation += 2
-        self.advance_and_print('(')
+        self.advance_and_print(['('])
         self.compile_parameter_list()
-        self._tokenizer.go_back() # fix faulty printing of closing bracket
-        self.advance_and_print(')')
+        self._tokenizer.go_back()  # fix faulty printing of closing bracket
+        self.advance_and_print([')'])
         self._xml_output.append(' ' * self._indentation + "<subroutineBody>")
         self._indentation += 2
-        self.advance_and_print('{')
-        while self._tokenizer.advance() and self._tokenizer.return_token_value == "var":
+        self.advance_and_print(['{'])
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() == "var":
             self._tokenizer.go_back()
             self.compile_var_dec()
         else:
-            self._xml_output.append(' ' * self._indentation + "<statements>")
+            self._tokenizer.go_back()
             self.compile_statements()
-            self._xml_output.append(' ' * self._indentation + "</statements>")
+        self._indentation -= 2
         self._xml_output.append(' ' * self._indentation + "</subroutineBody>")
-        self._xml_output.append("</subroutineDec>")
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</subroutineDec>")
 
     def compile_parameter_list(self):
         # note subroutineDec ends with subRoutineBody
         self._xml_output.append(' ' * self._indentation + "<parameterList>")
         self._indentation += 2
-        self.advance_and_print(')')
+        self.advance_and_print([')'])
         self._indentation -= 2
         self._xml_output.pop() # remove faulty indentation and position of closing bracket
         self._xml_output.append(' ' * self._indentation + "</parameterList>")
@@ -94,60 +118,169 @@ class CompilationEngine:
     def compile_var_dec(self):
         self._xml_output.append(' ' * self._indentation + "<varDec>")
         self._indentation += 2
-        self.advance_and_print(';')
+        self.advance_and_print([';'])
         self._indentation -= 2
         self._xml_output.append(' ' * self._indentation + "</varDec>")
 
     def compile_statements(self):
-        while self._tokenizer.advance():
+        self._xml_output.append(' ' * self._indentation + "<statements>")
+        self._indentation += 2
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() != '}':
             # choose next statement to compile based on keyword
             # let, if, while, do, return
             next_function = self._statement_compile_function_dict[self._tokenizer.return_token_value()]
             self._tokenizer.go_back()
             next_function()
-
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</statements>")
+        self._tokenizer.go_back()
+        self.advance_and_print(['}'])
 
     def compile_do(self):
         self._xml_output.append(' ' * self._indentation + "<doStatement>")
         self._indentation += 2
-        self.advance_and_print()
-        self._token_stack.append("</doStatement>")
+        # pushed subroutine name or instance method
+        self.advance_and_print(['('])
+        self.compile_expression_list()  # pushes closing bracket
+        self.advance_and_print([';'])
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</doStatement>")
 
     def compile_let(self):
-        self._xml_output.append("<letStatement>")
-        self._indentation.append(" ")
-        self._indentation.append(" ")
-        self._completion_symbol_stack.append(';')
-        self._token_stack.append("</letStatement>")
+        self._xml_output.append(' ' * self._indentation + "<letStatement>")
+        self._indentation += 2
+        self.advance_and_print(['[', '='])
+        if self._tokenizer.return_token_value() == '[':  # check for optional expression
+            self.compile_expression()  # expression will end with ']'
+            self.advance_and_print(['='])
+        self.compile_expression()  # expression will end with ';'
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</letStatement>")
 
     def compile_while(self):
-        self._xml_output.append("<whileStatement>")
-        self._indentation.append(" ")
-        self._indentation.append(" ")
-        self._completion_symbol_stack.append(';')
-        self._token_stack.append("</whileStatement>")
+        self._xml_output.append(' ' * self._indentation + "<whileStatement>")
+        self._indentation += 2
+        self.advance_and_print(['('])
+        self.compile_expression()
+        self.advance_and_print(['{'])
+        self.compile_statements()
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</whileStatement>")
 
     def compile_return(self):
-        self._xml_output.append("<returnStatement>")
-        self._indentation.append(" ")
-        self._indentation.append(" ")
-        self._completion_symbol_stack.append(';')
-        self._token_stack.append("</returnStatement>")
+        self._xml_output.append(' ' * self._indentation + "<returnStatement>")
+        self._indentation += 2
+        self.advance_and_print(["return"])
+        if self._tokenizer.advance() and self._tokenizer.return_token_value() != ';':
+            self._tokenizer.go_back()
+            self.compile_expression()
+        else:
+            self._tokenizer.go_back()
+            self.advance_and_print([';'])
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</returnStatement>")
 
     def compile_if(self):
-        self._xml_output.append("<ifStatement>")
-        self._indentation.append(" ")
-        self._indentation.append(" ")
-        self._completion_symbol_stack.append('}')
-        self._token_stack.append("</ifStatement>")
+        self._xml_output.append(' ' * self._indentation + "<ifStatement>")
+        self._indentation += 2
+        self.advance_and_print(['('])
+        self.compile_expression()
+        self.advance_and_print(['{'])
+        self.compile_statements()
+        if self._tokenizer.advance() and self._tokenizer.return_token_value() == "else":
+            self._tokenizer.go_back()
+            self.advance_and_print(['{'])
+            self.compile_statements()
+        else:
+            self._tokenizer.go_back()
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</ifStatement>")
 
     def compile_expression(self):
-        self._xml_output.append("<expression>")
-        self._indentation.append(" ")
-        self._indentation.append(" ")
-        self._completion_symbol_stack.append(')')
-        self._token_stack.append("</expression>")
+        self._xml_output.append(' ' * self._indentation + "<expression>")
+        self._indentation += 2
+        self.compile_term()
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() not in [')', ']', ',', ';']:
+            # check and push of to xml op
+            self._tokenizer.go_back()
+            self.advance_and_print((), 1)  # push the op to output
+            # push term to xml
+            self.compile_term()
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</expression>")
+        # push detected ending character or closing brace
+        self._tokenizer.go_back()
+        self.advance_and_print((), 1)
 
     def compile_term(self):
+        self._xml_output.append(' ' * self._indentation + "<term>")
+        self._indentation += 2
+        self.advance_and_print((), 1)
+        if self._tokenizer.return_token_value() == '(':  # evaluate expression
+            self.compile_expression()
+        elif self._tokenizer.return_token_value() in ['-', '~']:  # handle unary op
+            self.compile_term()
+        else:
+            self.advance_and_print((), 1)
+            if self._tokenizer.return_token_value() == '(':
+                self.compile_expression_list()
+            elif self._tokenizer.return_token_value() == '[':
+                self.compile_expression()
+            elif self._tokenizer.return_token_value() == '.':  # method call
+                self.advance_and_print((), 2)
+                if self._tokenizer.return_token_value() == '(':
+                    self.compile_expression_list()
+                else:
+                    # tokenizer should go back if it encounters expression terminating characters
+                    self._tokenizer.go_back()
+                    self._xml_output.pop()
+            else:
+                # tokenizer should go back if it encounters expression terminating characters
+                self._tokenizer.go_back()
+                self._xml_output.pop()
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</term>")
 
     def compile_expression_list(self):
+        close = ""
+        self._xml_output.append(' ' * self._indentation + "<expressionList>")
+        self._indentation += 2
+        while self._tokenizer.advance() and self._tokenizer.return_token_value() != ')':
+            self._tokenizer.go_back()
+            self.compile_expression()
+            if self._tokenizer.return_token_value() == ')':
+                break
+        else:
+            self._tokenizer.go_back()
+            self.advance_and_print([')'])
+        close = self._xml_output.pop()[2:]
+        self._indentation -= 2
+        self._xml_output.append(' ' * self._indentation + "</expressionList>")
+        if close:
+            self._xml_output.append(close)
+
+    def write_to_file(self):
+        with open(self._output_stream, 'w') as output:
+            output.write('\n'.join(self._xml_output))
+            output.write('\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError("Please give a file/directory path as argument")
+
+    input_path = sys.argv[1]
+    if os.path.isfile(input_path) and input_path.endswith(".jack"):
+        tokenizer = JackTokenizer(input_path)
+        compiler = CompilationEngine(tokenizer, input_path.replace(".jack", "Compiled.xml"))
+        compiler.write_to_file()
+    elif os.path.isdir(input_path):
+        for file in os.listdir(input_path):
+            if file.endswith(".jack"):
+                tokenizer = JackTokenizer(os.path.join(input_path, file))
+                compiler = CompilationEngine(tokenizer,
+                                             os.path.join(input_path,file.replace(".jack", "Compiled.xml")),
+                                             )
+                compiler.write_to_file()
+    else:
+        raise ValueError("incorrect path given")
